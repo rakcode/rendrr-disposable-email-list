d0ce700d66a537c8b239dc9a4e73513da26a61d7
Rakesh DL <rdlbirthday@gmail.com>
2019-09-05 02:12:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const config = require("./config");
+
+module.exports.isDisposable = (email) => {
+    let result = {isEmail: null, isDomain: null, isDisposable: null};
+
+    isEmail = config.isEmail(email);
+    if(isEmail) {
+        result.isEmail = true;
+        result.isDomain = false;
+        email = email.split("@")[1];
+    }else{
+        isDomain = config.isDomain(email);
+        if(isDomain) {
+            result.isDomain = true;
+            result.isEmail = false;
+        }
+    }
+
+    result.isDisposable = config.isDisposable(email);
+
+    return result;
+
+}
